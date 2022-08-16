36da01fa20c1eec0a3e58a810057feb00438caa2
Ilya Khazan <p9067700518@gmail.com>
2022-08-16 17:42:29 +0300

    create core components murkup

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
-<template></template>
+<template>
+  <div class="container">
+    <AddItemBlock />
+    <ItemList />
+  </div>
+</template>
 
-<script></script>
+<script>
+import AddItemBlock from "../components/AddItemBlock.vue";
+import ItemList from "../components/ItemList.vue";
+export default { components: { AddItemBlock, ItemList } };
+</script>
 
-<style></style>
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 32px;
+}
+</style>
